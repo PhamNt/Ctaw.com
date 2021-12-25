b4ee3b6d685e0fc04604f8180c0b4dfaa9a17f9e
datvn21 <dinhtiendat0201@gmail.com>
2021-12-25 07:49:55 +0700

    Remove Trash & fix bug

--- src/component/posT.js
+++ src/component/posT.js
@@ -1,33 +1,35 @@
-import React from 'react'
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
 class ListPost extends React.Component {
-    state = {
-        ListUser: []
-    }
-    async componentDidMount() {
-        let res = await axios.get("https://wibuki-api.herokuapp.com/api/getposts")
-        this.setState({
-            ListUser: res && res.data && res.data.data ? res.data : []
-        })
-    }
-    render() {
-        let { ListUser } = this.state;
-        return (
-            <div className='container'>
-                <div> This is list user </div>
-                {ListUser && ListUser.length > 0 &&
-                    ListUser.map((item, index) => {
-                        return (
-                            <div key={item.id}>
-                                {index + 1} {item.author} {item.title} {item.content}
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        )
-    }
+  state = {
+    ListUser: [],
+  };
+  async componentDidMount() {
+    let res = await axios.get("https://wibuki-api.herokuapp.com/api/getposts");
+    console.log(res);
+    this.setState({
+      ListUser: res.data,
+    });
+  }
+  render() {
+    let { ListUser } = this.state;
+    console.log(ListUser);
+    return (
+      <div className="container">
+        <div> This is list user </div>
+        {ListUser &&
+          ListUser.length > 0 &&
+          ListUser.map((item, index) => {
+            return (
+              <div key={item.id}>
+                {index + 1} {item.author} {item.title} {item.content}
+              </div>
+            );
+          })}
+      </div>
+    );
+  }
 }
 
 export default ListPost;
